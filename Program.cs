f7141ed9a31e030170f67c244269040da1dacef6
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-08-21 20:37:16 -0400

    add comment for possible swagger implementation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+//For the .Net core project, you need to install Four packages. Better to use Nuget package manager so it takes appropriate needed versions.
+
+// Microsoft.OpenApi;
+// Swashbuckle.AspNetCore.Swagger;
+// Swashbuckle.AspNetCore.SwaggerGen;
+// Swashbuckle.AspNetCore.SwaggerUI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,6 +20,8 @@ if (!app.Environment.IsDevelopment())
 {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    // app.UseSwagger();
+    // app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
